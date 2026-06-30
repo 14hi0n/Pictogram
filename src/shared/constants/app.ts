fc72aa34e39aa0222fe1fcda6ee16bcfb6b4ffd1
Octoober <kirill.job2019@gmail.com>
2026-06-30 23:27:48 +0700

    chore: bump version to v1.2.0-alpha

--- src/shared/constants/app.ts
+++ src/shared/constants/app.ts
@@ -1,5 +1,5 @@
 export const APP_NAME = 'Pictogram';
 
 export const PROJECT_LINKS = {
-	github: 'https://github.com/Octoober/Pictogram',
+	github: 'https://github.com/14hi0n/Pictogram',
 } as const;
